7343499f5913bba57f407b08ab9e9f8d461431e2
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-12-08 11:22:24 -0500

    draft of cryptfolio

--- crypfolio.py
+++ crypfolio.py
@@ -1,8 +1,43 @@
 import yaml
+import requests
+
+FILENAME = 'portfolio.yaml'
+API_ROOT = 'https://api.coinmarketcap.com/v1/ticker/'
+FIXER_CAD_URL = 'https://api.fixer.io/latest?base=USD'
+FORMAT = ['symbol', 'amount', 'price_usd', 'total_usd', 'percent_change_24h']
 
 if __name__ == '__main__':
-    with open('portfolio.yaml', 'r') as stream:
+    # Open the portfolio yaml file
+    with open(FILENAME, 'r') as stream:
         try:
-            print yaml.load(stream)
+            portfolio = yaml.load(stream)
         except yaml.YAMLError as exc:
             print exc
+
+    # Get the data from coinmarketcap
+    data = [FORMAT]
+    total = 0
+    for crypto in portfolio:
+        info = requests.get(API_ROOT + crypto).json()[0]
+
+        # Update the total
+        total += portfolio[crypto] * float(info['price_usd'])
+
+        # Formatting so that the output looks cleaner
+        info['amount'] = portfolio[crypto]
+        info['total_usd'] = info['amount'] * float(info['price_usd'])
+        info['total_usd'] = "${:.2f}".format(info['total_usd'])
+        info['percent_change_24h'] += '%'
+
+        data += [map(lambda x: str(info[x]), FORMAT)]
+
+    # Print the data cleanly
+    widths = [max([len(item) for item in col]) for col in zip(*data)]
+    fmt = ''.join(['{{:{}}}'.format(width + 4) for width in widths])
+    for row in data:
+        print(fmt.format(*row))
+    print 'Total USD: %s' % "${:.2f}".format(total)
+
+    # Get the USD -> CAD conversion rate
+    exchange_rate = requests.get(FIXER_CAD_URL).json()['rates']['CAD']
+    print 'Total CAD: %s' % "${:.2f}".format(total * exchange_rate)
